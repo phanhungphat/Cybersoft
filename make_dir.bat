c85ae563a3a7295bc2937fb2a646979ed8e69c20
phanhungphat <phanphat.programming@gmail.com>
2025-09-09 00:33:54 +0700

    add some docs

--- make_dir.bat
+++ make_dir.bat
@@ -1,21 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Lặp từ 1 đến 32
-for /L %%i in (1,1,32) do (
-    rem Định dạng số có 2 chữ số (01, 02, ..., 32)
-    set "num=0%%i"
-    set "num=!num:~-2!"
-
-    rem Tạo thư mục Buoi_xx
-    mkdir "Buoi_!num!"
-
-    rem Tạo thư mục con
-    mkdir "Buoi_!num!\de_bai"
-    mkdir "Buoi_!num!\bai_lam"
-
-    rem Tạo file README.md
-    echo # Buoi_!num! > "Buoi_!num!\README.md"
+rem Lặp qua tất cả thư mục bắt đầu bằng "Buoi_"
+for /d %%f in (Buoi_*) do (
+    mkdir "%%f\tai_lieu"
 )
 
 echo Done!
